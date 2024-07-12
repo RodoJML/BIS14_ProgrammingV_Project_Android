9d13291981a97bc3f6b1755b4f63f5c29484da47
Rodolfo <rodojml@me.com>
2024-07-12 17:26:15 -0600

    Adding loginlayoug to mainActivity.kt

--- app/src/main/java/com/example/habittracker/MainActivity.kt
+++ app/src/main/java/com/example/habittracker/MainActivity.kt
@@ -1,33 +1,36 @@
 package com.example.habittracker
 
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import androidx.activity.ComponentActivity
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.habittracker.database.BackgroundTask
-import com.example.habittracker.ui.theme.HabitTrackerTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var UsernameEt: EditText
+    private lateinit var PasswordEt: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.logindb_layout)
 
-        val userNameEt = findViewById<EditText>(R.id.dbuser_field)
-        val passwordEt = findViewById<EditText>(R.id.dbpassword_field)
+        UsernameEt = findViewById<EditText>(R.id.dbuser_field)
+        PasswordEt = findViewById<EditText>(R.id.dbpassword_field)
 
-        fun onLogin() {
-            val userName = userNameEt.text.toString()
-            val password = passwordEt.text.toString()
-            val type = "login"
-            val backgroundTask = BackgroundTask(this)
-            backgroundTask.execute(type, userName, password)
-        }
+
+    }
+
+    fun onLogin(view: View) {
+        val userName = UsernameEt.text.toString()
+        val password = PasswordEt.text.toString()
+        val type = "login"
+        val backgroundTask = BackgroundTask(this)
+        backgroundTask.execute(type, userName, password)
     }
 }
 
+
+/*
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
 
@@ -43,4 +46,4 @@ fun GreetingPreview() {
     HabitTrackerTheme {
         Greeting("Android")
     }
-}
+}*/
